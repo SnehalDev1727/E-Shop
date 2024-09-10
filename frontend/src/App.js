cb167950f632307fa2e97644fb94e1c85a710f95
Snehal Satpute <snehal.s@sphinxworldbiz.com>
2024-09-10 12:48:49 +0530

    ChatandProductdetails

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// components:
+import HeaderComponent from "./components/HeaderComponent";
+import FooterComponent from "./components/FooterComponent";
+
+//user components:
+import RoutesWithUserChatComponent from "./components/user/RoutesWithUserChatComponent";
+
 // publicly available pages:
 import HomePage from "./pages/HomePage";
 import ProductDetailsPage from "./pages/ProductDetailsPage";
@@ -8,12 +16,6 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 
 import ProtectedRoutesComponent from "./components/ProtectedRoutesComponent";
-import HeaderComponent from "./components/HeaderComponent";
-import FooterComponent from "./components/FooterComponent";
-
-//user component
-import RoutesWithUserChatComponent from "./components/user/RoutesWithUserChatComponent";
-
 
 // protected user pages:
 import UserProfilePage from "./pages/user/UserProfilePage";
@@ -31,13 +33,15 @@ import AdminOrdersPage from "./pages/admin/AdminOrdersPage";
 import AdminOrderDetailsPage from "./pages/admin/AdminOrderDetailsPage";
 import AdminChatsPage from "./pages/admin/AdminChatsPage";
 import AdminAnalyticsPage from "./pages/admin/AdminAnalyticsPage";
+import ScrollToTop from "./utils/ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
+    <ScrollToTop />
       <HeaderComponent />
       <Routes>
-        <Route element={<RoutesWithUserChatComponent admin={false} />}>
+        <Route element={<RoutesWithUserChatComponent />}>
           {/* publicly available routes: */}
           <Route path="/" element={<HomePage />} />
           <Route path="/product-list" element={<ProductListPage />} />
@@ -47,21 +51,19 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="*" element="Page not exists 404" />
-
-          {/* <Route path="/" component={HomePage} />  in previous versions of react-router-dom */}
-
-
         </Route>
-          {/* user protected routes: */}
-          <Route element={<ProtectedRoutesComponent admin={false} />}>
-            <Route path="/user" element={<UserProfilePage />} />
-            <Route path="/user/my-orders" element={<UserOrdersPage />} />
-            <Route path="/user/cart-details" element={<UserCartDetailsPage />} />
-            <Route
-              path="/user/order-details"
-              element={<UserOrderDetailsPage />}
-            />
-          </Route>
+        {/* <Route path="/" component={HomePage} />  in previous versions of react-router-dom */}
+
+        {/* user protected routes: */}
+        <Route element={<ProtectedRoutesComponent admin={false} />}>
+          <Route path="/user" element={<UserProfilePage />} />
+          <Route path="/user/my-orders" element={<UserOrdersPage />} />
+          <Route path="/user/cart-details" element={<UserCartDetailsPage />} />
+          <Route
+            path="/user/order-details"
+            element={<UserOrderDetailsPage />}
+          />
+        </Route>
 
         {/* admin protected routes: */}
         <Route element={<ProtectedRoutesComponent admin={true} />}>
